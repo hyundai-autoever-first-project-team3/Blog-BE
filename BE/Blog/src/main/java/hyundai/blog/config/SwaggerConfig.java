bde670ca5f0d553ba69f8c1f24813d5a8fa5f496
Zoonmy <pjm2571@naver.com>
2024-09-26 12:30:38 +0900

    settings: Swagger 기본 설정
    - SpringDoc 2.6.0 버전을 이용한 Swagger UI 구성

---
+++ BE/Blog/src/main/java/hyundai/blog/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package hyundai.blog.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String TITLE = "백엔드 API 명세";
+    private static final String VERSION = "0.1";
+    private static final String DESCRIPTION = "블로그 프로젝트 백엔드 API 명세서입니다.";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(TITLE)
+                        .version(VERSION)
+                        .description(DESCRIPTION));
+    }
+}
