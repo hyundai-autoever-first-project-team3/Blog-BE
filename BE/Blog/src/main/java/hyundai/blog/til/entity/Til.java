2c07ac92bc1261ae7301a7e40ce0336db836ec2e
Zoonmy <pjm2571@naver.com>
2024-10-21 11:33:22 +0900

    refactor: Til 클래스의 userId를 memberId로 변경

--- BE/Blog/src/main/java/hyundai/blog/til/entity/Til.java
+++ BE/Blog/src/main/java/hyundai/blog/til/entity/Til.java
@@ -18,8 +18,8 @@ public class Til {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id")
-    private Long userId;
+    @Column(name = "member_id")
+    private Long memberId;
 
     private String language;
     private String site;
