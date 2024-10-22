af82cb1a86d49935f041d479d04d628b6b12240b
eunsol <eunsol037@naver.com>
2024-10-22 17:49:12 +0900

    feat : challengeTil에 siteKinds 추가, challengeTil 3개 조회 구현

--- BE/Blog/src/main/java/hyundai/blog/challenge/repository/ChallengeTilRepository.java
+++ BE/Blog/src/main/java/hyundai/blog/challenge/repository/ChallengeTilRepository.java
@@ -1,7 +1,12 @@
 package hyundai.blog.challenge.repository;
 
+import hyundai.blog.challenge.entity.Challenge;
 import hyundai.blog.challenge.entity.ChallengeTil;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChallengeTilRepository extends JpaRepository<ChallengeTil, Long> {
+
+    List<ChallengeTil> findAllByChallengeId(Long challengeId);
 }
