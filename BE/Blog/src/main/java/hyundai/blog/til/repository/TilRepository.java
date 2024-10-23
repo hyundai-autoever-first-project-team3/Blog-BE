fa1900ba7dc3cf06859a4bbb51b83e70f3c5ad81
Zoonmy <pjm2571@naver.com>
2024-10-23 14:56:56 +0900

    feat: mypage tils 조회 기능 구현

--- BE/Blog/src/main/java/hyundai/blog/til/repository/TilRepository.java
+++ BE/Blog/src/main/java/hyundai/blog/til/repository/TilRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TilRepository extends JpaRepository<Til, Long> {
 
     Page<Til> findAll(Pageable pageable);
+
+    Page<Til> findAllByMemberId(Long memberId, Pageable pageable);
 }
